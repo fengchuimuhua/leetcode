a80e8088b37d04584f49d571a499b39f95780b88
zhangrunfeng <zhangrunfeng@meituan.com>
2020-02-19 11:30:11 +0800

    ALDS1_8_C

--- src/main/java/aoj/ALDS1_8_A/Main.java
+++ src/main/java/aoj/ALDS1_8_A/Main.java
@@ -15,7 +15,7 @@ public class Main {
         Node left;
         Node right;
 
-        public Node (long val) {
+        public Node(long val) {
             this.val = val;
             this.left = null;
             this.right = null;
@@ -49,6 +49,75 @@ public class Main {
         if (root.right != null) {
             sb.append(" ");
             preOrder(root.right, sb);
+        }
+    }
+
+    private boolean find(long targetVal) {
+        Node tmp = root;
+        while (tmp != null) {
+            if (tmp.val == targetVal) {
+                return true;
+            } else if (tmp.val < targetVal) {
+                tmp = tmp.right;
+            } else {
+                tmp = tmp.left;
+            }
+        }
+        return false;
+    }
+
+    private boolean delete(long targetVal) {
+        Node pre = null, cur = root;
+        while (cur != null && cur.val != targetVal) {
+            pre = cur;
+            if (targetVal < cur.val) {
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+
+        if (cur == null) {
+            return false;
+        }
+
+        if (!(cur.left != null && cur.right != null)) {
+            // 待删除结点至多只有一个子结点
+            Node childNode;
+            if (cur.left != null) {
+                childNode = cur.left;
+            } else {
+                childNode = cur.right;
+            }
+            cur.left = null;
+            cur.right = null;
+
+            if (pre == null) {
+                root = childNode;
+                return true;
+            } else {
+                if (pre.left == cur) {
+                    pre.left = childNode;
+                } else {
+                    pre.right = childNode;
+                }
+                return true;
+            }
+        } else {
+            // 待删除结点包含左右两个子结点
+            Node preStar = cur, curStar = cur.right;
+            while (curStar.left != null) {
+                preStar = curStar;
+                curStar = curStar.left;
+            }
+            cur.val = curStar.val;
+            if (preStar.left == curStar) {
+                preStar.left = curStar.right;
+            } else {
+                preStar.right = curStar.right;
+            }
+            curStar.right = null;
+            return true;
         }
     }
 
@@ -88,6 +157,17 @@ public class Main {
                 sb.setLength(0);
                 preOrder(root, sb);
                 System.out.println(" " + sb.toString());
+            } else if (str.equals("find")) {
+                long targetVal = sc.nextLong();
+                boolean ifFind = find(targetVal);
+                if (ifFind) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+            } else if (str.equals("delete")) {
+                long target = sc.nextLong();
+                delete(target);
             }
         }
     }
